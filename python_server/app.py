7c300da34a924dd19a49c69aee302fec1b5f20d9
leandroTorres <leandrohenrique_@live.com>
2026-02-10 18:32:19 -0300

    add server

---
+++ python_server/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/user/<user_id>')
+def get_user(user_id):
+    return f"this is the user {user_id}"
+
+if __name__ == '__main__':
+    # We host on 0.0.0.0 so it's accessible outside the container
+    app.run(host='0.0.0.0', port=1111)
